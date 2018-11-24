bb8b4e056d7e95a15431dcdfa129fdecd82ecf1a
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2018-11-24 17:50:46 -0600

    Closes #72. Change ratio variables to rank-hot encoded variables.
    Change all the original ordinal variables that were converted into
    ratios into rank-hot encoded variables. Now all variables are of
    binary type and are of equal footing for machine learning.

--- nobel_physics_prizes/src/features/features_utils.py
+++ nobel_physics_prizes/src/features/features_utils.py
@@ -1,11 +1,14 @@
+import pandas as pd
+
+
 def convert_categoricals_to_numerical(features):
     """Convert categorical features to numerical features.
 
     Args:
-        features (pandas.Dataframe): Features dataframe.
+        features (pandas.DataFrame): Features dataframe.
 
     Returns:
-        pandas.Dataframe: Features dataframe with categorical
+        pandas.DataFrame: Features dataframe with categorical
             values ['yes', 'no'] and ['male', 'female']
             replaced with [1, 0].
     """
@@ -29,3 +32,60 @@ def convert_target_to_numerical(target):
 
     target_numerical = target.map({'yes': 1, 'no': 0})
     return target_numerical
+
+
+def rank_hot_encode(features, encoder, columns=None):
+    """Rank-hot encode ordinal features.
+
+    Rank-hot encode ordinal features in the spirit of:
+        http://scottclowe.com/2016-03-05-rank-hot-encoder/
+    This code is drastically adapted from the code at which no longer works:
+        https://stackoverflow.com/questions/50329109/rank-hot-encoding-python3
+
+    Args:
+        features (pandas.DataFrame): Features dataframe.
+        encoder (sklearn.preprocessing.OneHotEncoder): One hot encoder. The fit
+            method should have been previously called on the encoder.
+        columns (list or pandas.Series, optional): Defaults to None. List of
+            columns to encode.
+
+    Returns:
+        pandas.DataFrame: Features dataframe with rank-hot encoded values.
+            The original columns are dropped.
+    """
+
+    if not columns:
+        columns = features.columns
+
+    encoded_columns = [
+        '{}_at_least_{}'.format(col_name, val)
+        for col_name, cat in zip(features[columns], encoder.categories_)
+        for val in cat
+    ]
+
+    one_hot = encoder.transform(features[columns])
+    rank_hot = one_hot.copy()
+
+    col_start = 0
+    cols_to_drop = []
+    for _, cat in enumerate(encoder.categories_):
+        cols_to_drop.append(col_start)
+        col_end = col_start + len(cat)
+        pos_first_zeros = (one_hot[:, col_start:col_end] != 0).argmax(axis=1)
+        for row, pos_first_one in enumerate(pos_first_zeros):
+            if pos_first_one == 0:
+                continue
+            # set all values before the first one to a one
+            rank_hot[row, col_start:col_start + pos_first_one] = 1
+        col_start = col_end
+
+    enc_features = pd.DataFrame(
+        rank_hot, index=features.index, columns=encoded_columns)
+    # drop the first columns as they add no information (a value of exactly zero will
+    # have all columns equal to zero)
+    enc_features = enc_features.drop(
+        enc_features.columns[cols_to_drop], axis='columns')
+
+    features_with_rank_hot = features.drop(columns, axis='columns')
+    features_with_rank_hot = features_with_rank_hot.join(enc_features)
+    return features_with_rank_hot
--- tests/src/features/test_features_utils.py
+++ tests/src/features/test_features_utils.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import pytest
+from sklearn.preprocessing import OneHotEncoder
 
 from src.features.features_utils import (convert_categoricals_to_numerical,
-                                         convert_target_to_numerical)
+                                         convert_target_to_numerical, rank_hot_encode)
 
 
 @pytest.fixture()
@@ -28,6 +29,45 @@ def expected_target():
     return expected
 
 
+@pytest.fixture()
+def ordinal_features():
+    ordinal = pd.DataFrame(data=[
+        [1, 3, 0],
+        [0, 1, 1],
+        [2, 4, 2]
+    ], index=[0, 1, 2], columns=['ord_1', 'ord_2', 'ord_3'])
+    return ordinal
+
+
+@pytest.fixture()
+def binary_features():
+    binary = pd.DataFrame(data=[
+        [1],
+        [0],
+        [0]
+    ], index=[0, 1, 2], columns=['bin_1'])
+    return binary
+
+
+@pytest.fixture()
+def expected_rank_hot_encode():
+    ordinal = pd.DataFrame(data=[
+        [1, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [1, 1, 1, 1, 1, 1]
+    ], dtype='int64', index=[0, 1, 2], columns=[
+        'ord_1_at_least_1', 'ord_1_at_least_2',
+        'ord_2_at_least_3', 'ord_2_at_least_4',
+        'ord_3_at_least_1', 'ord_3_at_least_2'])
+    return ordinal
+
+
+@pytest.fixture()
+def expected_rank_hot_encode_columns(expected_rank_hot_encode, binary_features):
+    expected = binary_features.join(expected_rank_hot_encode)
+    return expected
+
+
 def test_convert_categoricals_to_numerical(expected_features):
     features = pd.DataFrame(data=[
         ['Sheldon Cooper', 1.4, 'male', 'yes'],
@@ -43,3 +83,22 @@ def test_convert_target_to_numerical(expected_target):
     target = expected_target.map({1: 'yes', 0: 'no'})
     target_numerical = convert_target_to_numerical(target)
     assert(target_numerical.equals(expected_target))
+
+
+def test_rank_hot_encode_all_features(ordinal_features, expected_rank_hot_encode):
+    enc = OneHotEncoder(categories='auto', sparse=False,
+                        dtype='int64', handle_unknown='ignore')
+    enc.fit(ordinal_features)
+    rank_hot = rank_hot_encode(ordinal_features, enc)
+    assert(rank_hot.equals(expected_rank_hot_encode))
+
+
+def test_rank_hot_encode_columns(binary_features, ordinal_features,
+                                 expected_rank_hot_encode_columns):
+    enc = OneHotEncoder(categories='auto', sparse=False,
+                        dtype='int64', handle_unknown='ignore')
+    mixed_features = ordinal_features.join(binary_features)
+    enc.fit(mixed_features[ordinal_features.columns])
+    rank_hot = rank_hot_encode(
+        mixed_features, enc, columns=ordinal_features.columns.tolist())
+    assert(rank_hot.equals(expected_rank_hot_encode_columns))
