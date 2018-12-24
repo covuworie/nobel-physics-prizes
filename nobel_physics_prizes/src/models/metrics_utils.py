241b1f6f42fbb28384750f6c19bb0a47397e7d73
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2018-12-23 23:21:27 -0600

    Closes #81. Model selection.
    Produce a plot of Matthews Correlation Coefficient (MCC) vs (binary)
    classification threshold for each of the classifiers. For SVM transform
    the decision function threshold to a value between 0 and 1 so it is on
    the same scale as the other classifiers. Define and compute the
    "normalized area under the MCC (NAUC MCC) curve" and select the model
    with the highest value.

--- nobel_physics_prizes/src/models/metrics_utils.py
+++ nobel_physics_prizes/src/models/metrics_utils.py
@@ -1,4 +1,9 @@
+import warnings
+
+import numpy as np
 import pandas as pd
+from sklearn.metrics import auc, matthews_corrcoef, roc_curve
+from sklearn.preprocessing import binarize
 
 
 def print_matthews_corrcoef(corrcoef, classifier_name, data_label='train'):
@@ -53,3 +58,103 @@ def confusion_matrix_to_dataframe(
     confusion_matrix_df.loc[column_total_label, index_total_label] = (
         confusion_matrix_df.sum()[index_total_label])
     return confusion_matrix_df.astype('int64')
+
+
+def mcc_curve(y_true, y_score, pos_label=None, sample_weight=None, drop_intermediate=True, probability=True):
+    """Compute Matthews correlation coefficient (MCC) curve.
+
+    Note: this implementation is restricted to the binary classification task.
+
+    Args:
+        y_true (array, shape = [n_samples]): True binary labels. If labels are not either {-1, 1} or {0, 1},
+            then pos_label should be explicitly given.
+        y_score (array, shape = [n_samples]): Target scores, can either be probability estimates of the
+            positive class, confidence values, or non-thresholded measure of decisions (as returned by 
+            `decision_function` on some classifiers).
+        pos_label (int or str, optional): Defaults to None. Label considered as positive and others are
+            considered negative.
+        sample_weight (array-like of shape = [n_samples], optional): Defaults to None. Sample weights.
+        drop_intermediate (bool, optional): Defaults to True. Whether to drop some suboptimal thresholds
+            which would not appear on a plotted MCC curve. This is useful in order to create lighter MCC curves.
+        probability (bool, optional): Defaults to True. Whether `y_score` are probability estimates of the
+            positive class. If True then the thresholds are bounded in [0, 1]. The `sklearn` learn value of
+            thresholds[0] from `roc_curve` is replaced by 1.0 and a value is appended to the end of thresholds
+            such that thresholds[-1] is equal to 0 (and the false positive and true positive rates are set to 1.0). 
+
+    Returns:
+        mccs : array, shape = [>2]
+            Matthews Correlation Coefficients such that element i is the MCC of predictions with
+            score >= thresholds[i].
+
+        thresholds : array, shape = [n_thresholds]
+            Decreasing thresholds on the decision function used to compute MCC.
+    """
+
+    fpr, tpr, thresholds = roc_curve(y_true=y_true, y_score=y_score, pos_label=pos_label,
+                                     sample_weight=sample_weight, drop_intermediate=drop_intermediate)
+
+    # If y_score is a probability then bound the thresholds between 0 and 1. Set the first (highest)
+    # value to 1 and append a last (lowest) value equal to 0.
+    # See: https://github.com/scikit-learn/scikit-learn/issues/3097
+    if probability:
+        thresholds[0] = 1.0
+        thresholds = np.append(thresholds, 0.0)
+        fpr = np.append(fpr, 1.0)
+        tpr = np.append(tpr, 1.0)
+
+    mccs = []
+    with warnings.catch_warnings():  # ignore runtime warnings caused by zero MCC
+        warnings.filterwarnings('ignore', category=RuntimeWarning)
+        for threshold in thresholds:
+            y_pred = binarize(y_score.reshape(-1, 1),
+                              threshold=threshold).astype('int64')
+            mcc = matthews_corrcoef(
+                y_true, y_pred, sample_weight=sample_weight)
+            mccs.append(mcc)
+
+    return np.array(mccs), thresholds
+
+
+def mcc_auc_score(y_true, y_score, sample_weight=None, probability=True, normalize=True):
+    """Compute Area Under the Matthews Correlation Coefficient Curve (MCC AUC) from prediction scores.
+
+    Note: this implementation is restricted to the binary classification task or multilabel classification 
+    task in label indicator format.
+
+    Args:
+        y_true (array, shape = [n_samples] or [n_samples, n_classes]): True binary labels or binary label
+            indicators.
+        y_score (array, shape = [n_samples] or [n_samples, n_classes]): Target scores, can either be
+            probability estimates of the positive class, confidence values, or non-thresholded measure of
+            decisions (as returned by `decision_function` on some classifiers). For binary y_true, y_score
+            is supposed to be the score of the class with greater label.
+        sample_weight (array-like of shape = [n_samples], optional): Defaults to None. Sample weights.
+        probability (bool, optional): Defaults to True. Whether `y_score` are probability estimates of the
+            positive class. If True then the thresholds are bounded in [0, 1]. The `sklearn` learn value of
+            thresholds[0] from `roc_curve` is replaced by 1.0 and a value is appended to the end of thresholds
+            such that thresholds[-1] is equal to 0 (and the false positive and true positive rates are set to 1.0). 
+        normalize (bool, optional): Defaults to True. Whether to normalize the MCC AUC so that it bounded in
+            [0, 1]. The normalization constant is the maximum possible AUC across the support of the thresholds
+            (i.e. its minimum and maximum values). This is equivalent to assuming an MCC = 1.0 for all thresholds.
+            Note that for `probability`=True setting `normalize`=True should have no effect as the normalization
+            constant is equal to 1.0.
+
+    Returns:
+        auc: float
+            Area under the curve.
+    """
+
+    mccs, thresholds = mcc_curve(
+        y_true, y_score, sample_weight=sample_weight, probability=probability)
+    mcc_auc = auc(thresholds, mccs)
+
+    if not normalize:
+        return mcc_auc
+
+    mccs_ones = np.ones(2)
+    mcc_ones_auc = auc(np.array([thresholds[0], thresholds[-1]]), mccs_ones)
+    mcc_auc /= mcc_ones_auc
+    if probability:
+        assert(0.0 <= mcc_auc <= 1.0)
+
+    return mcc_auc
--- tests/src/models/test_metrics_utils.py
+++ tests/src/models/test_metrics_utils.py
@@ -1,9 +1,13 @@
+import warnings
+
 import numpy as np
 import pandas as pd
 import pytest
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import auc, confusion_matrix, matthews_corrcoef, roc_curve
+from sklearn.preprocessing import binarize
 
-from src.models.metrics_utils import confusion_matrix_to_dataframe
+from src.models.metrics_utils import (confusion_matrix_to_dataframe,
+                                      mcc_auc_score, mcc_curve)
 
 
 @pytest.fixture
@@ -32,6 +36,54 @@ def expected_confusion_matrix(expected_confusion_matrix_numpy):
     return expected
 
 
+@pytest.fixture
+def y_true_y_score():
+    y_true = np.array([0, 0, 1, 1])
+    y_score = np.array([0.1, 0.4, 0.35, 0.8])
+    return y_true, y_score
+
+
+@pytest.fixture
+def expected_roc_curve(y_true_y_score):
+    y_true, y_score = y_true_y_score
+    fpr, tpr, thresholds = roc_curve(y_true, y_score)
+    return fpr, tpr, thresholds
+
+
+@pytest.fixture
+def expected_roc_curve_probability(expected_roc_curve):
+    fpr, tpr, thresholds = expected_roc_curve
+    thresholds[0] = 1.0
+    thresholds = np.append(thresholds, 0.0)
+    fpr = np.append(fpr, 1.0)
+    tpr = np.append(tpr, 1.0)
+    return fpr, tpr, thresholds
+
+
+@pytest.fixture
+def expected_mcc_curve(y_true_y_score, expected_roc_curve):
+    y_true, y_score = y_true_y_score
+    _, _, thresholds = expected_roc_curve
+    mccs = []
+    for threshold in thresholds:
+        y_pred = (y_score > threshold).astype('int64')
+        mcc = matthews_corrcoef(y_true, y_pred)
+        mccs.append(mcc)
+    return np.array(mccs), thresholds
+
+
+@pytest.fixture
+def expected_mcc_curve_probability(y_true_y_score, expected_roc_curve_probability):
+    y_true, y_score = y_true_y_score
+    _, _, thresholds = expected_roc_curve_probability
+    mccs = []
+    for threshold in thresholds:
+        y_pred = (y_score > threshold).astype('int64')
+        mcc = matthews_corrcoef(y_true, y_pred)
+        mccs.append(mcc)
+    return np.array(mccs), thresholds
+
+
 def test_confusion_matrix_to_dataframe_default_values(
         expected_confusion_matrix_default):
     y_true = [0, 1, 0, 1]
@@ -52,3 +104,52 @@ def test_confusion_matrix_to_dataframe(
         index_total_label='Measured total',
         column_total_label='Classified total')
     assert(conf_matrix_df.equals(expected_confusion_matrix))
+
+
+@pytest.mark.filterwarnings('ignore::RuntimeWarning')
+def test_mcc_curve(y_true_y_score, expected_mcc_curve):
+    y_true, y_score = y_true_y_score
+    expected_mccs, expected_thresholds = expected_mcc_curve
+    mccs, thresholds = mcc_curve(y_true, y_score, probability=False)
+    np.testing.assert_allclose(thresholds, expected_thresholds)
+    np.testing.assert_allclose(mccs, expected_mccs)
+
+
+@pytest.mark.filterwarnings('ignore::RuntimeWarning')
+def test_mcc_curve_probability(
+        y_true_y_score, expected_mcc_curve_probability):
+    y_true, y_score = y_true_y_score
+    expected_mccs, expected_thresholds = expected_mcc_curve_probability
+    mccs, thresholds = mcc_curve(y_true, y_score, probability=True)
+    np.testing.assert_allclose(thresholds, expected_thresholds)
+    np.testing.assert_allclose(mccs, expected_mccs)
+
+
+@pytest.mark.filterwarnings('ignore::RuntimeWarning')
+def test_mcc_auc_score(y_true_y_score, expected_mcc_curve):
+    y_true, y_score = y_true_y_score
+    mccs, thresholds = expected_mcc_curve
+
+    expected_mcc_auc = auc(thresholds, mccs)
+    mcc_auc = mcc_auc_score(
+        y_true, y_score, probability=False, normalize=False)
+    np.testing.assert_allclose(mcc_auc, expected_mcc_auc)
+    mcc_auc = mcc_auc_score(y_true, y_score, probability=False, normalize=True)
+
+    normalized_thresholds = (
+        (thresholds - np.min(thresholds)) / (np.max(thresholds) - np.min(thresholds)))
+    expected_mcc_auc = auc(normalized_thresholds, mccs)
+    np.testing.assert_allclose(mcc_auc, expected_mcc_auc)
+
+
+@pytest.mark.filterwarnings('ignore::RuntimeWarning')
+def test_mcc_auc_score_probability(y_true_y_score, expected_mcc_curve_probability):
+    y_true, y_score = y_true_y_score
+    mccs, thresholds = expected_mcc_curve_probability
+
+    expected_mcc_auc = auc(thresholds, mccs)
+    mcc_auc = mcc_auc_score(y_true, y_score, probability=True, normalize=False)
+    np.testing.assert_allclose(mcc_auc, expected_mcc_auc)
+
+    mcc_auc = mcc_auc_score(y_true, y_score, probability=True, normalize=True)
+    np.testing.assert_allclose(mcc_auc, expected_mcc_auc)
