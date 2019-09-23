e61783d751c5d71268c480a0fe1daa82297aa1f8
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2019-09-23 11:36:18 -0500

    Closes #120. Create skeleton HTML markup for website.
    Create HTML markup for header and footer of each page.
    Add titles to each page along with meta description.
    Reset basic styling of the page.

---
+++ website/javascript/footer.js
@@ -0,0 +1,12 @@
+document.write('\
+\
+    <footer>\
+    <nav>\
+        <ul>\
+            <li><a href="techniques.html">Techniques</a></li>\
+            <li><a href="technology-stack.html">Technology Stack</a></li>\
+            <li><a href="2018-physics-nobel-laureates.html">Physics Nobel Laureates</a></li>\
+        </ul>\
+    </nav>\
+    </footer>\
+');
---
+++ website/javascript/header.js
@@ -0,0 +1,38 @@
+document.write('\
+\
+    <header>\
+        <nav>\
+            <ul>\
+                <li><a href="index.html">Home</a></li>\
+                    <ul>\
+                        <li><span>Notebooks ▾</span></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/0.0-introduction.html">Introduction</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/1.0-collect-nobel-prize-laureates.html">Collect Nobel Prize Laureates</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/1.1-collect-physicists.html">Collect Physicists</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/1.2-collect-physicists-raw-data.html">Collect Physicists Raw Data</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/2.0-process-physicists-raw-data.html">Process Physicists Raw Data</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/2.1-train-val-test-split.html">Train-Validation-Test Split</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/2.2-collect-places-raw-data.html">Collect Places Raw Data</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/2.3-process-places-raw-data.html">Process Places Raw Data</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/2.4-reverse-geocode-places-interim-data.html">Reverse Geocode Places Interim Data</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/3.0-build-features.html">Build Features</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/3.1-build-target.html">Build Target</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/4.0-exploratory-data-analysis.html">Exploratory Data Analysis</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/4.1-exploratory-factor-analysis.html">Exploratory Factor Analysis</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/4.2-topic-modeling.html">Topic Modeling</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/5.0-baseline-model.html">Baseline Model</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/5.1-covariate-shift.html">Covariate Shift</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/5.2-importance-weighting.html">Importance Weighting</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/5.3-train-model.html">Train Model</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/5.4-predict-model.html">Predict Model</a></li>\
+                        <li><a href="../nobel_physics_prizes/notebooks/html_output/6.0-conclusion.html">Conclusion</a></li>\
+                    </ul>\
+                </li>\
+                <li><a href="demographics.html">Demographics</a></li>\
+                <li><a href="data-sources.html">Data Sources</a></li>\
+                <li><a href="2018-physics-nobel-laureates.html">2018 Physics Nobel Laureates</a></li>\
+            </ul>\
+        </nav>\
+    </header>\
+\
+');
