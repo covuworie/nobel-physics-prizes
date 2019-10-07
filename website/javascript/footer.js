07b4e4574ec1b14dc383396131da4b1b7bd05686
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2019-10-07 13:38:06 -0500

    Closes #134. Add external links.

--- website/javascript/footer.js
+++ website/javascript/footer.js
@@ -1,9 +1,19 @@
 document.write('\
     <footer>\
     <nav>\
+        <h4>Internal Links:<\h4>\
         <ul>\
             <li><a href="techniques.html">Techniques</a></li>\
-            <li><a href="technology-stack.html">Technology Stack</a></li>\
+            <li><a href"technology-stack.html">Technology Stack</a></li>\
+        </ul>\
+        <h4>External Links:<\h4>\
+        <ul>\
+            <li><a href="https://www.nobelprize.org/" target="_blank">Nobel Prize Organisation</a></li>\
+            <li><a href="https://www.nobelprize.org/prizes/lists/all-nobel-prizes-in-physics" target="_blank">All Nobel Prizes in Physics</a></li>\
+            <li><a href="https://www.nobelprize.org/nomination/redirector/?redir=archive/" target="_blank">Nobel Prize Nomination Archive</a></li>\
+            <li><a href="https://wiki.dbpedia.org" target="_blank">DBPedia</a></li>\
+            <li><a href="https://en.wikipedia.org/wiki/List_of_physicists" target="_blank">List of physicists</a></li>\
+            <li><a href="https://en.wikipedia.org/wiki/List_of_theoretical_physicists" target="_blank">List of theoretical physicists</a></li>\
         </ul>\
     </nav>\
     <p>Copyright © Chuk Ovuworie 2019. All rights reserved.</p>\
